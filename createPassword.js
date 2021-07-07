e356d6b0809907241dead1abe06af05e0000136c
ch2isk4kos <ch2isk4kos@gmail.com>
2021-07-07 18:38:11 -0400

    test functionality to create a password

--- createPassword.js
+++ createPassword.js
@@ -1,6 +1,22 @@
-const characterss =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=+[]{}|<>,.?/";
-
 const createPassword = (length) => {
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=+[]{}|<>,.?/";
   let chars = characters;
+  return generatePassword(length, chars);
 };
+
+const generatePassword = (length, chars) => {
+  let pw = "";
+  for (let i = 0; i < length; i++) {
+    // let char = Math.floor(Math.random() * chars.length + 1);
+    // pw += chars.charAt(char);
+
+    pw += chars.charAt(Math.floor(Math.random() * chars.length + 1));
+  }
+  console.log(pw);
+  return pw;
+};
+
+// createPassword(50);
+
+module.exports = createPassword;
