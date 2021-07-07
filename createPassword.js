85be8ceee64688a821e1fedea8932af8e4d677e8
ch2isk4kos <ch2isk4kos@gmail.com>
2021-07-07 18:01:33 -0400

    add file for functionality to create a new password

---
+++ createPassword.js
@@ -0,0 +1,6 @@
+const characterss =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=+[]{}|<>,.?/";
+
+const createPassword = (length) => {
+  let chars = characters;
+};
