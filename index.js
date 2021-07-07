8a5fd1cf49f3359d636e4941a15e42a870f66bf9
ch2isk4kos <ch2isk4kos@gmail.com>
2021-07-07 16:15:28 -0400

    import commander module and initialize program

---
+++ index.js
@@ -0,0 +1,18 @@
+const program = require("commander");
+
+program
+  .version("1.0.0")
+  .description("Commander Password Generator Demo")
+  .parse();
+
+// from the console: node index -h
+
+/*
+  Usage: index [options]
+
+  Commander Password Generator Demo
+
+  Options:
+    -V, --version  output the version number
+    -h, --help     display help for command
+*/
