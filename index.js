f952bf5d1d380f9cf075c4596def92f00998e9f9
ch2isk4kos <ch2isk4kos@gmail.com>
2021-07-07 18:08:32 -0400

    import and test createPassword functionality

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const program = require("commander");
+const createPassword = require("./createPassword");
 
-program
-  .version("1.0.0")
-  .description("Commander Password Generator Demo")
-  .parse();
+const program = require("commander"); // code
+
+program.version("1.0.0").description("Commander Password Generator Demo"); // code
 
 // from the console: node index -h
 
@@ -16,3 +15,70 @@ program
     -V, --version  output the version number
     -h, --help     display help for command
 */
+
+// program
+//   .command("generate")
+//   .action(() => {
+//     console.log("Generated!");
+//   })
+//   .parse(); // code
+
+// from the console: node index generate
+
+/*
+  Generated!
+*/
+
+// program.option("-len, --length <number>", "length of password", "10").parse(); // code
+
+// from the console: node index -h
+
+/*
+Usage: index [options]
+
+Commander Password Generator Demo
+
+Options:
+-V, --version   output the version number
+-len, --length  length of password
+-h, --help      display help for command
+*/
+
+// console.log(program.opts()); // code
+
+// from the console: node index
+
+/*
+  { length: '10' }
+*/
+
+// from the console: node index --length=24
+
+/*
+  { length: '24' }
+*/
+
+// program
+//   .option("-len, --length <number>", "length of password", "10")
+//   .option("-s, --save", "save password to db")
+//   .option("-!n, --no-numbers", "generate password without numbers")
+//   .option("-!sym, --no-symbols", "generate password without symbols")
+//   .parse(); // code
+
+// console.log(program.opts()); // code
+
+// from the console: node index --length=24 --save
+
+/*
+{ length: '24', save: true }
+*/
+
+// program
+//   .option("-len, --length <number>", "length of password", "10")
+//   .option("-s, --save", "save password to db")
+//   .parse(); // code
+
+const { length, save, num, sym } = program.opts();
+
+const generatePassword = createPassword(length);
+console.log(generatePassword);
